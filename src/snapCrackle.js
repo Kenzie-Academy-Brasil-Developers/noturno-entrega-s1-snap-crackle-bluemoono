a5b999bbee6644ffe0d53f61bf659f98694452d0
Bluemoono <mikka_paulina@live.com>
2021-10-20 17:11:04 -0300

    Criação de lógica e criação de index

--- src/snapCrackle.js
+++ src/snapCrackle.js
@@ -1,1 +1,23 @@
 // Seu código vai aqui
+
+function snapCrackle(maxValue){
+    let a = '';
+    for(let i=1; i<=maxValue;i++){        
+        if (i%2!==0&&i%5===0) {
+            a+= "SnapCrackle, ";
+            continue;
+        } else if (i%5===0) {
+            a+= 'Crackle, ';
+            continue;
+        } if (i%2!==0) {
+            a+='Snap, ';
+            continue;
+        } else {
+            a+=i + ', '
+            continue;
+        }
+    }
+    document.write(a)
+
+}
+snapCrackle(15)
